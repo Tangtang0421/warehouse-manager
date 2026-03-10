1737eb44749f8ae79d231067ed778026aa34ab9c
xys <210967231@qq.com>
2026-03-10 15:59:49 +0800

    加入mybatis-plus支持

--- OA/src/main/java/com/qlx/oa/OaApplication.java
+++ OA/src/main/java/com/qlx/oa/OaApplication.java
@@ -1,9 +1,11 @@
 package com.qlx.oa;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qlx.oa.mapper")
 public class OaApplication {
 
     public static void main(String[] args) {
--- OA/src/main/java/com/qlx/oa/controller/HelloController.java
+++ OA/src/main/java/com/qlx/oa/controller/HelloController.java
@@ -1,12 +1,23 @@
 package com.qlx.oa.controller;
 
+import com.qlx.oa.entity.User;
+import com.qlx.oa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
     @GetMapping
     public String hello(){
         return "Hello World";
+    }
+    @Autowired
+    private UserService userService;
+    @GetMapping("/list")
+    public List<User> list(){
+        return userService.list();
     }
 }
